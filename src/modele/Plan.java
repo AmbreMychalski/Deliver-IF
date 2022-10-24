71da4efd4eb9bf8fa2da4e119ba6a42dc43c9810
MatheoJ <matheo.jospeph.01@gmail.com>
2022-10-24 19:46:21 +0200

    add compute smallest path in Plan

--- src/modele/Plan.java
+++ src/modele/Plan.java
@@ -2,7 +2,10 @@ package modele;
 
 import java.io.File;
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Set;
 import java.util.List;
 import java.util.Map;
 import java.util.regex.Pattern;
@@ -31,11 +34,57 @@ public class Plan {
 	private String nom;
 	private Map<Long, Intersection> intersections =  new HashMap<Long, Intersection>();
 	private List<Segment> segments =  new ArrayList<Segment>();
+	private Map<Long, List<Segment>> intersectionsNeighbours = new HashMap<Long, List<Segment>>();
 	
 	
 	public Plan(File fichier) {
 		this.parseXML(fichier);
 	}
+	
+	public HashMap<Intersection, Float> calculerPlusCourtsChemins(List<Intersection> listIntersections, Intersection depart){
+        HashMap<Long, Float> distance = new HashMap<Long, Float>();
+        Set<Long> intersectionsAVerifier = new HashSet<Long>();
+        Set<Long> intersectionsGrises = new HashSet<Long>();
+        Set<Long> intersectionsNoires = new HashSet<Long>();
+        for(var entry : intersections.entrySet()) {
+            distance.put(entry.getKey(),-1.0f);
+        }
+        distance.put(depart.getIdIntersection(), 0.0f);
+        
+        for(Intersection inter: listIntersections) {
+            intersectionsAVerifier.add(inter.getIdIntersection());
+        }
+                
+        intersectionsGrises.add(depart.getIdIntersection());
+        
+        while(intersectionsAVerifier.size()!=0 && intersectionsGrises.size()!=0) {
+            
+            Long currentInter = obtenirIntersectionLaPlusProche(intersectionsGrises, distance);
+            System.out.println("");
+            intersectionsGrises.remove(currentInter);
+            for(Segment seg : intersectionsNeighbours.get(currentInter) ) {
+                Intersection voisin = seg.getDestination();
+                Long idVoisin = voisin.getIdIntersection();
+                if(!intersectionsNoires.contains(idVoisin)) {
+                    if(distance.get(currentInter)+seg.getLongueur()<distance.get(idVoisin)
+                            ||distance.get(idVoisin)==-1) {
+                        distance.put(idVoisin,distance.get(currentInter)+seg.getLongueur());
+                    }                   
+                    intersectionsGrises.add(idVoisin);
+                }
+            }
+            intersectionsGrises.remove(currentInter);
+            intersectionsAVerifier.remove(currentInter);
+            intersectionsNoires.add(currentInter);          
+        }
+        
+        HashMap<Intersection, Float>res = new HashMap<Intersection, Float>();
+        for(Intersection inter: listIntersections) {
+            res.put(inter, distance.get(inter.getIdIntersection()));
+        }       
+        
+        return res;
+    }
 
 	public void parseXML(File fichier) {
 		nom = fichier.getName().split(Pattern.quote("."))[0];
@@ -82,6 +131,10 @@ public class Plan {
 						longueur = Float.parseFloat(attributs.getNamedItem("length").getNodeValue());
 						
 						segments.add(new Segment(this.intersections.get(origineId), this.intersections.get(destinationId), longueur, nom));
+						if(!this.intersectionsNeighbours.containsKey(origineId)) {
+                            this.intersectionsNeighbours.put(origineId, new ArrayList<Segment>());
+                        }
+                        this.intersectionsNeighbours.get(origineId).add(segments.get(segments.size()-1));
 					}
 				}
 			}
@@ -94,5 +147,18 @@ public class Plan {
 			this.entrepot = null;
 		}
 	}
+
+	private Long obtenirIntersectionLaPlusProche(Set < Long > intersectionsGrise, HashMap<Long, Float> distance ) {
+        Long interPlusProche = null;
+        float plusPetiteDist = Float.MAX_VALUE;
+        for (Long idInter: intersectionsGrise) {
+            float interDist = distance.get(idInter);
+            if (interDist < plusPetiteDist) {
+                plusPetiteDist = interDist;
+                interPlusProche = idInter;
+            }
+        }
+        return interPlusProche;
+    }
 }
 
