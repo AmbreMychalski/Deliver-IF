282f820e5d4a95a0747e9081f6b477758de231b9
LudivineK <96302236+LudivineK@users.noreply.github.com>
2022-10-28 08:38:11 +0200

    Correction packages

--- src/main/demarrer/Deliverif.java
+++ src/main/demarrer/Deliverif.java
@@ -8,20 +8,43 @@ package main.demarrer;
 
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import main.controleur.ControleurFenetrePrincipale;
 
 /**
  * Point d'entrée de l'applicaiton Deliverif.
  * @author H4113
  *
  */
-public class Deliverif {
+public class Deliverif extends Application {
 
     /**
      * Lance l'application.
      * @param args
      */
 	public static void main(String[] args) {
-		Application.launch(LanceurApplication.class, args);
+		Application.launch(Deliverif.class, args);
 	}
+	
+	@Override
+    public void start(Stage primaryStage) throws Exception {
+        try {
+            
+            FXMLLoader loader = new FXMLLoader(getClass()
+                    .getResource("/FenetrePrincipale.fxml"));
+            Parent root = (Parent)loader.load();
+            ControleurFenetrePrincipale controleur = loader.getController();
+            controleur.setStage(primaryStage);
+            primaryStage.setTitle("Calcul de tournées");
+            primaryStage.setScene(new Scene(root));
+            primaryStage.show();
+            
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
+    }
 
 }
