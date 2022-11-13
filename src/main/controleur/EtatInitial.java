d067a1fe09dc5986ea57a9bfbcd906c722b392b1
julianamartin <julimf123@gmail.com>
2022-11-13 17:55:54 +0100

    Début de l'implémentation DP Commande

--- src/main/controleur/EtatInitial.java
+++ src/main/controleur/EtatInitial.java
@@ -3,12 +3,8 @@ package controleur;
 import java.io.File;
 
 import exception.FichierNonConformeException;
-import javafx.scene.canvas.GraphicsContext;
 import javafx.stage.FileChooser;
-import modele.Intersection;
 import modele.Plan;
-import modele.Segment;
-import vue.VueFenetrePrincipale;
 
 public class EtatInitial extends Etat{
     public EtatInitial() {
@@ -27,44 +23,15 @@ public class EtatInitial extends Etat{
         System.out.println("Fichier choisi = " + fichier.getAbsolutePath());
 
         try {
-            GraphicsContext gc = c.vue.canvasPlan.getGraphicsContext2D();
-            gc.clearRect(0, 0, c.vue.canvasPlan.getWidth(), c.vue.canvasPlan.getHeight());
-
+//            GraphicsContext gc = c.vue.canvasPlan.getGraphicsContext2D();
+//            gc.clearRect(0, 0, c.vue.canvasPlan.getWidth(), c.vue.canvasPlan.getHeight());
             c.planCharge = new Plan(fichier);
             c.journee.setPlan(c.planCharge);
 
-            c.vue.latMax = c.planCharge.getIntersections().values().stream()
-                    .map(Intersection::getLatitude)
-                    .max(Float::compare).orElse(0f);
-            c.vue.latMin = c.planCharge.getIntersections().values().stream()
-                    .map(Intersection::getLatitude)
-                    .min(Float::compare).orElse(0f);
-            c.vue.longMax = c.planCharge.getIntersections().values().stream()
-                    .map(Intersection::getLongitude)
-                    .max(Float::compare).orElse(0f);
-            c.vue.longMin = c.planCharge.getIntersections().values().stream()
-                    .map(Intersection::getLongitude)
-                    .min(Float::compare).orElse(0f);
+            Command com = new PlanCommand (c);
+            com.undoCommand();
+            com.doCommand();
 
-            c.vue.largeurPlan = c.vue.longMax - c.vue.longMin;
-            c.vue.hauteurPlan = c.vue.latMax - c.vue.latMin;
-
-            /*c.vue.echelle = Math.min(c.vue.canvasPlan.getWidth() / c.vue.largeurPlan,
-                    c.vue.canvasPlan.getHeight() / c.vue.hauteurPlan);*/
-            c.vue.echelleLong = c.vue.canvasPlan.getWidth() / c.vue.largeurPlan;
-            c.vue.echelleLat = c.vue.canvasPlan.getHeight() / c.vue.hauteurPlan;
-
-            for (Segment segment : c.planCharge.getSegments()) {
-                c.vue.dessinerSegment(segment,
-                        c.vue.COULEUR_SEGMENT);
-            }
-
-            c.vue.dessinerIntersection(c.vue.canvasPlan.getGraphicsContext2D(),
-                    c.getJournee().getPlan().getEntrepot(),
-                    c.vue.COULEUR_DEPOT,
-                    c.vue.TAILLE_CERCLE_INTERSECTION,
-                    true,
-                    VueFenetrePrincipale.FormeIntersection.CERCLE);
             c.vue.titledPaneEditionDemande.setVisible(true);
             c.vue.titlePaneSelectionDemande.setVisible(true);
             c.changementEtat(c.etatSansDemande);
