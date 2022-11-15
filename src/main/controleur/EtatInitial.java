53c063e92793fad7d2fed5ee71dab92c2e95b1bd
AmbreMychalski <ambre.mychalski@gmail.com>
2022-11-15 11:17:23 +0100

    enlèvement d'éléments de débuggages inutiles (oubli)

--- src/main/controleur/EtatInitial.java
+++ src/main/controleur/EtatInitial.java
@@ -44,28 +44,14 @@ public class EtatInitial extends Etat {
             c.vue.textfieldPlageHoraire.setText("");
             this.resetLabelRuesIntersection(c);
             if(c.journee.getLivreurs().get(0).getDemandeLivraisons().size() !=0){
-                System.out.println("ici");
-                /*for(Livreur livreur : c.journee.getLivreurs()){
-                    for(DemandeLivraison ligne : livreur.getDemandeLivraisons()) {
-                        livreur.supprimerDemandeLivraison(ligne);
-                    }
-                    for(Livraison ligne : livreur.getLivraisons()){
-                        livreur.supprimerLivraison(ligne);
-                    }
-                }
-                c.journee.setLivreurs(new ArrayList<>());*/
                 c.journee = new Journee();
                 c.journee.ajouterObservateur(c.vue);
                 Livreur.reinitializeNbLivreurs();
                 Livreur liv = new Livreur();
                 liv.ajouterObservateur(c.vue);
                 c.journee.getLivreurs().add(liv);
-
-                System.out.println(liv);
-
                 c.etatCourant.majComboboxLivreur(c);
                 c.vue.comboboxLivreur.getSelectionModel().selectFirst();
-                System.out.println(c.journee);
             }
             Plan plan;
             try {
